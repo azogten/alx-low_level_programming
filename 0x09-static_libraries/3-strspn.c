b7dc1bb3e4a513672d1f9cd5de1822c291139bf9
azogten <96079943+azogten@users.noreply.github.com>
2022-09-30 17:45:13 +0100

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -8,21 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int c = 0;
-char *t = accept;
+	unsigned int c = 0;
+	char *t = accept;
 
-while (*s++)
-{
-while (*accept++)
-if (*(s - 1) == *(accept - 1))
-{
-c++;
-break;
-}
-if (!(*--accept))
-break;
-accept = t;
-}
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
 
-return (c);
+	return (c);
 }
