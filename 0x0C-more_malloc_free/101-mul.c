f57b9962df563def71765993b2e941edd8c84234
azogten <teniolata@gmail.com>
2022-10-06 05:51:52 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -76,7 +76,7 @@ for (len1 = len1 - 1; len1 >= 0; len1--)
 digit1 = s1[len1] - '0';
 carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-{	  
+{
 digit2 = s2[len2] - '0';
 carry += result[len1 + len2 + 1] + (digit1 * digit2);
 result[len1 + len2 + 1] = carry % 10;
