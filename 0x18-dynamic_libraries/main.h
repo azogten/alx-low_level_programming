169d37a9ee82e1217cde84daf6930e77ceb696f4
azogten <teniolata@gmail.com>
2022-12-12 07:01:14 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int add(int a, int b);
+
+int sub(int a, int b);
+
+int mul(int a, int b);
+
+int div(int a, int b);
+
+int mod(int a, int b);
+
+int _putchar(char c);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int _abs(int n);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int _strlen(char *s);
+
+void _puts(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
